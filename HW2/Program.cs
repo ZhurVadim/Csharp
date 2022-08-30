e07ec67b2b0f0cca3ff236b51ad41db5535eb3da
ZhurVadim <worker2022@yandex.ru>
2022-08-30 13:22:04 +0800

    Создали файл

---
+++ HW2/Program.cs
@@ -0,0 +1,58 @@
+﻿Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого
+числа.
+
+Random random = new Random();  
+int randomNumber = random.Next(100, 1000);
+Console.WriteLine(randomNumber);
+
+int SecondNum(int num)
+{
+int SecondNumber = (randomNumber % 100) / 10;
+
+return SecondNumber;
+}
+
+int result =  SecondNum(randomNumber);
+Console.WriteLine(result);
+
+
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Random random = new Random();  
+int randomNumber = random.Next();
+Console.WriteLine(randomNumber);
+
+
+int ThirdNumber(int num)
+{
+  int ThirdNumber =   
+}
+
+
+
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool Day(int num)
+{
+    if(num < 6)
+    return true;
+    return false;
+}
+
+
+bool result = Day(num);
+if(result == true)
+{
+    Console.WriteLine("Невыходной");
+}
+else
+{
+    Console.WriteLine("Выходной");
+}
+
+
