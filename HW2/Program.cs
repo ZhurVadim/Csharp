cbe373c97f31039eb91ebcb9f19c4f30eb67b5da
ZhurVadim <worker2022@yandex.ru>
2022-08-30 13:33:33 +0800

    Решение задачи 13

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,22 +1,22 @@
-﻿Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого
-числа.
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого
+// числа.
 
-Random random = new Random();  
-int randomNumber = random.Next(100, 1000);
-Console.WriteLine(randomNumber);
+// Random random = new Random();  
+// int randomNumber = random.Next(100, 1000);
+// Console.WriteLine(randomNumber);
 
-int SecondNum(int num)
-{
-int SecondNumber = (randomNumber % 100) / 10;
+// int SecondNum(int num)
+// {
+// int SecondNumber = (randomNumber % 100) / 10;
 
-return SecondNumber;
-}
+// return SecondNumber;
+// }
 
-int result =  SecondNum(randomNumber);
-Console.WriteLine(result);
+// int result =  SecondNum(randomNumber);
+// Console.WriteLine(result);
 
 
-Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 Random random = new Random();  
 int randomNumber = random.Next();
@@ -25,34 +25,41 @@ Console.WriteLine(randomNumber);
 
 int ThirdNumber(int num)
 {
-  int ThirdNumber =   
+    while (num / 100 > 9)
+    {
+        num = num / 10;
+    }
+    int ThirdNumber = num%10;
+    return ThirdNumber;
 }
 
+int result =  ThirdNumber(randomNumber);
+Console.WriteLine(result);
 
 
-Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
-и проверяет, является ли этот день выходным.
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
 
 
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-bool Day(int num)
-{
-    if(num < 6)
-    return true;
-    return false;
-}
+// bool Day(int num)
+// {
+//     if(num < 6)
+//     return true;
+//     return false;
+// }
 
 
-bool result = Day(num);
-if(result == true)
-{
-    Console.WriteLine("Невыходной");
-}
-else
-{
-    Console.WriteLine("Выходной");
-}
+// bool result = Day(num);
+// if(result == true)
+// {
+//     Console.WriteLine("Невыходной");
+// }
+// else
+// {
+//     Console.WriteLine("Выходной");
+// }
 
 
